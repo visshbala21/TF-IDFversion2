038ae89e5e1a20f70ce5f8b5c2a171c364b32ff3
vishwa <visshbala12@gmail.com>
2023-07-01 10:23:16 -0500

    fixed the code so each document has a unique tf-idf score

--- tf_idfversion2fsmayne.py
+++ tf_idfversion2fsmayne.py
@@ -1,5 +1,6 @@
 import math
 from collections import Counter
+
 def calculate_tf_idf(documents):
     term_frequencies = []
     for doc in documents:
@@ -7,6 +8,7 @@ def calculate_tf_idf(documents):
         total_words = len(doc.split())
         tf = {word: count / total_words for word, count in word_counts.items()}
         term_frequencies.append(tf)
+
     document_count = len(documents)
     idf = {}
     for doc in documents:
@@ -16,41 +18,32 @@ def calculate_tf_idf(documents):
                 idf[word] += 1
             else:
                 idf[word] = 1
+
     idf = {word: math.log(document_count / freq) for word, freq in idf.items()}
+
     tfidf_scores = []
     for i, doc in enumerate(documents):
-        tfidf = {}
-        for word in doc.split():
-            tfidf[word] = term_frequencies[i][word] * idf[word]
+        tfidf = sum(term_frequencies[i][word] * idf[word] for word in doc.split())
         tfidf_scores.append(tfidf)
 
     return tfidf_scores
 
-def rank_pages(tfidf_scores, keyword):
-    
-    scores = {}
-    for i, tfidf in enumerate(tfidf_scores):
-        if keyword in tfidf:
-            scores[i] = tfidf[keyword]
-
-    
-    sorted_docs = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+def rank_pages(tfidf_scores):
+    sorted_docs = sorted(enumerate(tfidf_scores), key=lambda x: x[1], reverse=True)
     return sorted_docs
 
-
+# Read input from a file
 with open('input_file.txt', 'r') as file:
     content = file.read()
-pages = content.split('\n\n')
+    pages = content.split('\n\n')
 
-
+# Calculate TF-IDF scores for the pages
 tfidf_scores = calculate_tf_idf(pages)
 
+# Rank the pages based on the TF-IDF scores
+ranked_pages = rank_pages(tfidf_scores)
 
-keyword = input("Enter a keyword: ")
-
-ranked_pages = rank_pages(tfidf_scores, keyword)
-
-
-print("Ranked Pages : ")
+# Print the ranked pages
+print("Ranked Pages:")
 for page, score in ranked_pages:
     print(f"Page {page + 1}: {score}")
